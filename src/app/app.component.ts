9bf75d116e9dd6f5d17ba249140d64e89ae15261
Ain Mutaqorrobin <robin@mesiniaga.com.my>
2024-02-17 11:48:50 +0800

    code 11:48 A.M

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{ type: `server`, name: `Test`, content: `content test` }];
-  
-  onServerAdded(serverData: {serverName:string,serverContent:string}) {
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
       type: 'server',
       name: serverData.serverName,
-      content: serverData.serverContent
+      content: serverData.serverContent,
     });
   }
 
-  onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
-      content: blueprintData.serverContent
+      content: blueprintData.serverContent,
     });
+  }
+
+  onButtonClicked() {
+    this.serverElements[0].name = `Changed!`;
   }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css',
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked
+{
   @Input(`srvElement`)
   element: { type: string; name: string; content: string };
+  @Input(`name`) name: string;
 
-  constructor() {}
+  constructor() {
+    console.log(`constructor called`);
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(change: SimpleChanges): void {
+    console.log(`ngOnChanges called`);
+    console.log(change);
+  }
+  ngOnInit(): void {
+    console.log(`ngOnInit called`);
+  }
+
+  ngDoCheck(): void {
+    console.log(`ngDoCheck called`);
+  }
+
+  ngAfterContentInit(): void {
+    console.log(`ngAfterContentInit called`);
+  }
+  ngAfterContentChecked(): void {
+    console.log(`ngAfterContentCheck called`);
+  }
 }
